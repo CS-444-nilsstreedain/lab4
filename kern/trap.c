1858b15df3516d95cd1ebb0597189ab5dc7557d5
Nils <streedan@oregonstate.edu>
2023-11-19 11:47:25 -0800

    Completed Excersice 6

--- kern/trap.c
+++ kern/trap.c
@@ -200,6 +200,9 @@ trap_dispatch(struct Trapframe *tf)
     case T_PGFLT:
         page_fault_handler(tf);
         return;
+    case T_BRKPT:
+        monitor(tf);
+        return;
     default:
         break;
     }
